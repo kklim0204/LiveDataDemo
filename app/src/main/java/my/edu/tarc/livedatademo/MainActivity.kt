470748c57fbb69c9fb1969d1f357d4afe586af30
kklim0204 <kklim0204@gmail.com>
2021-08-18 14:22:30 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/livedatademo/MainActivity.kt
@@ -0,0 +1,46 @@
+package my.edu.tarc.livedatademo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import my.edu.tarc.livedatademo.databinding.ActivityMainBinding
+import my.edu.tarc.livedatademo.models.AccountModel
+
+class MainActivity : AppCompatActivity() {
+    private val myAccount = AccountModel("A1234566","John", 500.00)
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myAccount = myAccount
+        binding.lifecycleOwner = this
+
+        //display()
+        //myAccount.balance.observe(this, {newBalance -> binding.tvBalance.text = newBalance.toString()})
+
+        binding.btnDeposit.setOnClickListener(){
+            val amount:Double  = binding.tfAmount.text.toString().toDouble()
+            myAccount.deposit(amount)
+
+        }
+
+        binding.btnWitdraw.setOnClickListener(){
+            val amount:Double  = binding.tfAmount.text.toString().toDouble()
+            myAccount.withdraw(amount)
+
+
+
+        }
+    }
+
+//    fun display(){
+//
+//        binding.tvAccNo.text = myAccout.accNo
+//        binding.tvName.text = myAccout.owner
+//        binding.tvBalance.text = myAccout.balance.value.toString()
+//
+//    }
+}
